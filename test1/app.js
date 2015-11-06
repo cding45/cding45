40a2814fe1b08f5ea7f316c4b3b071989b99e8e7
cding45 <cding45@hotmail.com>
2015-11-06 11:42:23 -0600

    Update app.js

--- test1/app.js
+++ test1/app.js
@@ -1,9 +1,31 @@
-function MyController($scope, $ionicBackdrop, $timeout) {
-  //Show a backdrop for one second
-  $scope.action = function() {
-    $ionicBackdrop.retain();
-    $timeout(function() {
-      $ionicBackdrop.release();
-    }, 1000);
-  };
-}
+angular.module('mySuperApp', ['ionic'])
+.controller(function($scope, $ionicActionSheet, $timeout) {
+
+ // Triggered on a button click, or some other target
+ $scope.show = function() {
+
+   // Show the action sheet
+   var hideSheet = $ionicActionSheet.show({
+     buttons: [
+       { text: '<b>Share</b> This' },
+       { text: 'Move' }
+     ],
+     destructiveText: 'Delete',
+     titleText: 'Modify your album',
+     cancelText: 'Cancel',
+     cancel: function() {
+          // add cancel code..
+        },
+     buttonClicked: function(index) {
+       return true;
+     }
+   });
+
+   // For example's sake, hide the sheet after two seconds
+   $timeout(function() {
+     hideSheet();
+   }, 2000);
+
+ };
+});
+
